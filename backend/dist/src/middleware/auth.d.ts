15ec7bf12b999e362b71ae19805162a74d7c692f
Utkarsh Jain <183274388+UJ474@users.noreply.github.com>
2026-04-28 12:19:42 +0530

    hardcode DATABASE_URL in vercel config

--- backend/dist/src/middleware/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-export interface AuthRequest extends Request {
-    user?: {
-        id: string;
-        email: string;
-    };
-}
-export declare const authenticateToken: (req: AuthRequest, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=auth.d.ts.map
